43ea37c4946c2fa7eb3ce570896e1a1dbce90a05
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-06-25 10:26:22 -0700

    bugs fixed

--- djorg/settings.py
+++ djorg/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x&f)s267w+4mzjg7=4f_c*x(m7a!tu1(lrj^_-2$isfdb46d(8'
+SECRET_KEY =  config('SECRET_KEY')#'x&f)s267w+4mzjg7=4f_c*x(m7a!tu1(lrj^_-2$isfdb46d(8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)#True
 
 ALLOWED_HOSTS = []
 
