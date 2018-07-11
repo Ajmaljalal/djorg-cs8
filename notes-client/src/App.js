270bbdaffaabd1c8dcbce27eda2d71ce16957ca8
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-07-11 15:42:04 -0700

    notes-api deleted

--- notes-client/src/App.js
+++ notes-client/src/App.js
@@ -2,23 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-const notes = [
-  {
-    'id': 1,
-    'title': '1st Item',
-    'description': 'Description here.'
-  },
-  {
-    'id': 2,
-    'title': '2nd Item',
-    'description': 'Another description here.'
-  },
-  {
-    'id': 3,
-    'title': '3rd Item',
-    'description': 'Third description here.'
-  }
-];
 
 class App extends Component {
   state = {
